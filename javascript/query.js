6f34668708fb8aa986a454393cae572c6ac00a88
Bryan Sarmiento <51723446+bryansb@users.noreply.github.com>
2020-10-28 19:46:37 -0500

    Implementación de Paginación
    Se implementó la funcionalidad de paginación.

--- javascript/query.js
+++ javascript/query.js
@@ -1,11 +1,18 @@
 var sHero;
 var information;
-var accessToken = "3385995988148953";
+
+var maxPages;
+var currentPage = 0;
+
+var ACCESSTOKEN = "3385995988148953";
+var N = 1;
+
 var unavalaible = "<td id='hero_unavalaible'>No se pudo encontrar al superhéroe solicitado.</td>";
 var empty = "<td id='hero_empty'>Ingrese el nombre, o la identidad del superhéroe.</td>";
 
 function search(){
     sHero = document.getElementById("hero").value;
+    currentPage = 0;
 
     if (sHero == "") {
         document.getElementById("heros").innerHTML = empty;
@@ -22,7 +29,7 @@ function search(){
             }
         };
 
-        xmlhttp.open("GET","https://www.superheroapi.com/api.php/"+accessToken+"/search/"+sHero, true);
+        xmlhttp.open("GET","https://www.superheroapi.com/api.php/"+ACCESSTOKEN+"/search/"+sHero, true);
         xmlhttp.send();
     }
 }
@@ -57,16 +64,30 @@ function printInformation(){
     "  <th>Ojos</th>"+
     "  <th>Cabello</th>"+
     "</tr>"+
-  "</thead>";
+  "</thead>"+
+  "<tbody>";
 
     heroTable.innerHTML = header;
 
-    var infoHero;
+    maxPages = Math.trunc((information.results.length - 1) / N);
+    var min = currentPage * N;
+    var max = min + (N - 1);
 
-    for(var i=0; i < information.results.length; i++){
-        infoHero = "<tr>";
+    var infoHero = getHeroInfo(min, max, information);
 
-        infoHero += "<td>"+information.results[i].id+"</td>"+
+    printNavigation();
+
+    heroTable.innerHTML += infoHero + "</tbody>";
+
+}
+
+function getHeroInfo (minP, maxP, information) {
+    var heroes = "";    
+
+    for(var i=minP; i <= maxP; i++){
+        heroes += "<tr>";
+
+        heroes += "<td>"+information.results[i].id+"</td>"+
         "<td>"+information.results[i].name+"</td>"+
         "<td class='td-img' ><img src='"+information.results[i].image.url+"' alt='"+information.results[i].name+"' /></td>"+
         "<td>"+information.results[i].powerstats.intelligence+"</td>"+
@@ -86,14 +107,70 @@ function printInformation(){
         "<td>"+information.results[i].appearance['eye-color']+"</td>"+
         "<td>"+information.results[i].appearance['hair-color']+"</td>";
 
-        infoHero += "</tr>"
-       heroTable.innerHTML += infoHero;        
+        heroes += "</tr>"
     }
 
-    //heroTable.innerHTML += "</tbody>";
-
+    return heroes;
 }
 
-function paging(){
+function printNavigation () {
+    var container = document.getElementById("pages-numbers");
+    var navigation = " ";
 
+    // Posicion de inicio
+    if (currentPage == 0) {
+        navigation += "<a href='#' class='disabled' onclick='return false;'>Anterior</a>";
+    } else {
+        navigation += "<a href='#' class='normal' onclick='previousPage()'>Anterior</a>";
+    }
+
+    // Numeros intermedios
+    for (var i = 0; i <= maxPages; i++) {
+        if (i == currentPage) {
+            navigation += "<a href='#' class='selected'>"+(i+1)+"</a>";
+        } else {
+            navigation += "<a href='#' class='normal'>"+(i+1)+"</a>";
+        }
+    }
+
+    // Posicion al final
+    if (currentPage == maxPages) {
+        navigation += "<a href='#' class='disabled' onclick='return false;'>Siguiente</a>";
+    } else {
+        navigation += "<a href='#' class='normal' onclick='nextPage()'>Siguiente</a>";
+    }
+
+    container.innerHTML = navigation;
+}
+
+function nextPage(){
+    var aux = currentPage + 1;
+
+    if (aux > maxPages) {
+        currentPage = currentPage;
+    } else {
+        currentPage = aux;
+    }
+
+    printInformation();
+
+    return false;
+}
+
+function previousPage(){
+    var aux = currentPage - 1;
+
+    if (aux < 0) {
+        currentPage = 0;
+    } else {
+        currentPage = aux;
+    }
+
+    printInformation();
+
+    return false;
+}
+
+function currentPage() {
+    
 }
